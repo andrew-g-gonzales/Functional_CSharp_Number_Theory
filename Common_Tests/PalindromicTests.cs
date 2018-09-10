5c06e2cb0f66ae0426564ef7fed55a06b27ea541
andrew6000 <ag91375@gmail.com>
2018-09-09 20:43:13 -0400

    Updates...

--- Common_Tests/PalindromicTests.cs
+++ Common_Tests/PalindromicTests.cs
@@ -12,26 +12,25 @@ namespace Common_Tests
    
     public class PalindromicTests
     {
-        public static IEnumerable<object[]> Palindromes()
-       => (from i in Enumerable.Range(100, 900)
-           from j in Enumerable.Range(i, 1000 - i)
-           let k = i * j
-           where check(k.ToString())
-           select new object []{ k }).AsParallel()
-               .WithDegreeOfParallelism(Environment.ProcessorCount)
-               .WithExecutionMode(ParallelExecutionMode.ForceParallelism);
 
-        public static bool check(string input_number)
-        {
-            for (int i = 0; i < input_number.Length / 2; i++)
-                if (input_number[i] != input_number[input_number.Length - i - 1])
-                    return false;
-            return true;
-        }
+        public static IEnumerable<object[]> Palindromes5Digits() => 
+         (from i in (Enumerable.Range(100, 900).AsParallel()
+             .WithDegreeOfParallelism(Environment.ProcessorCount)
+              .WithExecutionMode(ParallelExecutionMode.ForceParallelism)
+              .WithMergeOptions(ParallelMergeOptions.NotBuffered))
+         from j in (Enumerable.Range(i, 1000- i))
+         let k = i * j
+         where check(k.ToString())
+         select new object[] { k });
+
+        public static bool check(string input) =>
+                  Enumerable.Range(0, input.Length / 2)
+                    .All(i => input[i] == input[input.Length - i - 1]);
+                  
 
         [Theory]
-        [MemberData(nameof(Palindromes))]
-        public void TestIsPalindrome3Digits(int integer)=> Assert.True(integer.IsPalindromic());
+        [MemberData(nameof(Palindromes5Digits))]
+        public void TestIsPalindrome5Digits(int integer) => Assert.True(integer.IsPalindromic());
 
     }
 }
