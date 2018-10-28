2dd8b3e4008388411af41dffd8965a0fd5dd022b
Andrew Gonzales <andrew.g.gonzales@avanade.com>
2018-10-28 11:09:36 -0400

    Removing hard coded numbers and replacing with parameters

--- Puzzles/DigitalCubes.cs
+++ Puzzles/DigitalCubes.cs
@@ -9,7 +9,7 @@ namespace Puzzles
     {
 
         public static IEnumerable<int> NumbersEqualToSumOfCubesFiltered(int start, int end, List<int> optimizedDigits) =>
-                 Enumerable.Range(100, 900)
+                 Enumerable.Range(start, end)
                 .Where(n => optimizedDigits.Contains(n.Digits().Sum()))
                 .ToDictionary(x => x, x => x.GetDigitsToPowerOf(3).Sum())
                 .Where(e => e.Key == e.Value)
@@ -17,7 +17,7 @@ namespace Puzzles
 
 
         public static IEnumerable<int> NumbersEqualToSumOfCubes(int start, int end) =>
-                Enumerable.Range(100, 900)
+                Enumerable.Range(start, end)
                .ToDictionary(x => x, x => x.GetDigitsToPowerOf(3).Sum())
                .Where(e => e.Key == e.Value)
                .Select(k => k.Key);
