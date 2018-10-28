92ce01a4ab66f3000474efcbdc2bb636a5ab6640
Andrew Gonzales <andrew.g.gonzales@avanade.com>
2018-10-28 18:55:37 -0400

    New projects for proofs of theorems

---
+++ Theorem_Test_Proofs/ModularArithmetic.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Common;
+
+namespace Theorem_Test_Proofs
+{
+    public class ModularArithmetic
+    {
+
+      public static IEnumerable<object[]> OddNumbersSquared() =>
+      (from i in (CustomRange.Int64(1,10000))
+       where i.IsOdd()
+       select new object[] { i.ToPowerOf(2) });
+
+
+        [Theory]
+        [MemberData(nameof(OddNumbersSquared))]
+        public void SquareOfOddNumber1MoewGreaterThanMultipleOf8(long oddNumberSquared)=> Assert.True((oddNumberSquared-1)%8 == 0L);
+        
+    }
+}
